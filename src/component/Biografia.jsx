d0d5fde58257f4075aa8c9b977512f52bf4a8576
JOSEBARZOLA <jose_barzola89@hotmail.com>
2022-12-22 02:02:24 -0300

    mediaquery

--- src/component/Biografia.jsx
+++ src/component/Biografia.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../component/MyStyle.css'
+import fotoPerfil from '../imagenes/imagenPerfil.jpg'
 
 function Biografia() {
   return (
     <>
     <div className='contenedorBiografia'>
+      <img className='imagen' src= {fotoPerfil} alt=""/>
         <p className='titulo'>¿Quien soy? </p>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam voluptatum modi expedita! Ex, dolor beatae reiciendis quaerat nulla voluptas. Quisquam quae perferendis at incidunt totam hic veritatis provident cupiditate aut
+    <p className='container'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam voluptatum modi expedita! Ex, dolor beatae reiciendis quaerat nulla voluptas. Quisquam quae perferendis at incidunt totam hic veritatis provident cupiditate aut
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam voluptatum modi expedita! Ex, dolor beatae reiciendis quaerat nulla voluptas. Quisquam quae perferendis at incidunt totam hic veritatis provident cupiditate aut
     </p>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam voluptatum modi expedita! Ex, dolor beatae reiciendis quaerat nulla voluptas. Quisquam quae perferendis at incidunt totam hic veritatis provident cupiditate aut
-    </p>
+    
     </div>
     </>
   )
